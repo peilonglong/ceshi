e063c5aaacbb8128770b31bbb9c83d8adbb760bd
peilonglong <577449440@qq.com>
2018-10-16 17:48:20 +0800

    push

--- ceshi/settings.py
+++ ceshi/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'ceshi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dailyfresh_bj18',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '172.16.179.130',
+        'PORT': 3306,
     }
 }
 
